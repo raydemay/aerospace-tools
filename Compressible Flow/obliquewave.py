3a6975a0c1482b5fa789750801aa0da704217937
Ray DeMay <rayjd08@gmail.com>
2022-04-06 22:37:17 -0400

    Renamed menu title

--- Compressible Flow/obliquewave.py
+++ Compressible Flow/obliquewave.py
@@ -1,7 +1,7 @@
 import sys
 import math
 
-print("ISENTROPIC FLOW CALCULATOR\n")
+print("OBLIQUE WAVE FLOW CALCULATOR\n")
 selection = int(
     input(
         "What is the known value?\n1: M1\n2: M2\n3: P2/P1\n4: rho2/rho1\n5: T2/T1\n6: p02/p01\n7: Deflection Angle\n8: Wave Angle\n9: Mn1\n10: Mn2\n\nEnter a number to continue: "
