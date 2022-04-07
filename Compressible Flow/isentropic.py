ba255dd46d767f0f4f04c85bfd62033294d4dd67
Ray DeMay <rayjd08@gmail.com>
2022-04-07 19:01:16 -0400

    Added Prandtl-Meyer function case

--- Compressible Flow/isentropic.py
+++ Compressible Flow/isentropic.py
@@ -9,20 +9,34 @@ selection = int(
 )
 knownValue = float(input("Enter the known value: "))
 gamma = float(input("Enter value for gamma: "))
-
 M = 0
+
+
+# Defining Prandtl-Meyer Function as a function since it used in multiple places
+def PrandtlMeyer(gamma, M):
+    nu = (
+        math.sqrt((gamma + 1) / (gamma - 1))
+        * math.atan((math.sqrt(((gamma - 1) / (gamma + 1)) * (M**2 - 1))))
+        - math.atan(math.sqrt(M**2 - 1))
+    ) * (180 / math.pi)
+    return nu
+
+
 # Determine Mach number from inputs
 if selection == 1:
     # Mach number is known
     M = knownValue
 elif selection == 2:
     # T0/T is known
+    T0_T = knownValue
     M = math.sqrt((knownValue - 1) * 2 / (gamma - 1))
 elif selection == 3:
     # P0/P is known
+    P0_P = knownValue
     M = math.sqrt(((knownValue ** ((gamma - 1) / gamma)) - 1) * 2 / (gamma - 1))
 elif selection == 4:
     # rho0/rho is known
+    R0_R = knownValue
     M = math.sqrt(((knownValue ** (gamma - 1)) - 1) * 2 / (gamma - 1))
 elif selection == 5:
     # A/A* known
@@ -50,9 +64,19 @@ elif selection == 5:
 elif selection == 6:
     mu = knownValue
     M = 1 / math.sin(mu)
+elif selection == 7:
+    # Prandtl-Meyer function known
+    nu = knownValue
+    M_guess = 2
+    while abs(M_guess - M) > 0.0000001:
+        M = M_guess
+        f = (PrandtlMeyer(gamma, M) - nu) * (math.pi / 180)  # needs to be in radians
+        fprime = math.sqrt(M**2 - 1) / (1 + (gamma - 1) * M**2 / 2) / M
+        M_guess = M - (f / fprime)
 else:
     print("error")
     sys.exit()
+
 
 # Calculate ratios and Mach angle from given/calculated Mach Number
 if selection != 2:
@@ -68,18 +92,16 @@ if selection != 5:
 if selection != 6:
     if M > 1:
         Mach_angle = math.asin(1 / M) * (180 / math.pi)
-        PrandtlMeyerAngle = (
-            math.sqrt((gamma + 1) / (gamma - 1))
-            * math.atan((math.sqrt(((gamma - 1) / (gamma + 1)) * (M**2 - 1))))
-            - math.atan(math.sqrt(M**2 - 1))
-        ) * (180 / math.pi)
+if selection != 7:
+    if M > 1:
+        nu = PrandtlMeyer(gamma, M)
     else:
         Mach_angle = 90
-        PrandtlMeyerAngle = 0
+        nu = 0
 print("Mach number is ", "{:.6f}".format(M))
 print("T0/T is ", "{:.6f}".format(T0_T))
 print("P0/P is ", "{:.6f}".format(P0_P))
 print("rho0/rho is", "{:.6f}".format(R0_R))
 print("A/A*", "{:.6f}".format(A_AStar))
 print("Mach angle is", "{:.6f}".format(Mach_angle), "degrees")
-print("Prandtl-Meyer angle is ", "{:.6f}".format(PrandtlMeyerAngle), "degrees")
+print("Prandtl-Meyer angle is ", "{:.6f}".format(nu), "degrees")
